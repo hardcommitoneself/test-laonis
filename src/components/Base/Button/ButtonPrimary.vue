f0d893dda15824cfa404f3b1afcf351a645c6957
hardcommitoneself <hardcommitoneself@gmail.com>
2022-07-30 02:18:16 -0700

    Add some base components

---
+++ src/components/Base/Button/ButtonPrimary.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :class="['flex items-center h-10 px-10 rounded-full cursor-pointer', active ? 'bg-blue-600 text-white' : ' bg-gray-100 text-black', ' hover:bg-blue-600 hover:text-white']">
+        <slot />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        active: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    }
+})
+</script>
---
+++ src/components/Base/Text/TextBase.vue
@@ -0,0 +1,24 @@
+<template>
+    <span :class="[fontWeight, fontSize]">
+        <slot />
+    </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        fontWeight: {
+            type: String,
+            required: false,
+            default: "font-normal"
+        },
+        fontSize: {
+            type: String,
+            required: false,
+            default: "text-base"
+        }
+    }
+})
+</script>
